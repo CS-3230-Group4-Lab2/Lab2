be4711d2a36174925787bfbc355224f9c6ac93a7
ClintFowler <jedd1876@gmail.com>
2015-06-01 20:26:57 -0600

    Adjusting Private to protected. Minor adjustments.

--- Lab2/src/lab2/StudentList.java
+++ Lab2/src/lab2/StudentList.java
@@ -5,7 +5,10 @@
  */
 package lab2;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -13,18 +16,23 @@ import java.util.List;
  */
 public class StudentList 
 {
-    private ChatResponses chats = new ChatResponses();
-    private List<Student> students;
-    private String[] roster;
+    protected ChatResponses chats = new ChatResponses();
+    protected List<Student> students;
+    protected String[] roster;
     
     public StudentList()
     {
-        roster = new String[]{"Kenneth Adair","Anthony Alder","Moselle Bair","Mathew Brewer","Michael Brown","Aaron Calder","Brett Carter","Tyler Cazier",
+        this.roster = new String[]{"Kenneth Adair","Anthony Alder","Moselle Bair","Mathew Brewer","Michael Brown","Aaron Calder","Brett Carter","Tyler Cazier",
             "Jared Elzinga","Clinton Fowler","Andrew Gray","Trever Marsh","Christopher Strickland","Ted Cowan","Justin Walker"};
         
-        for (String name; roseter)
+        Set<String> studentRoster = new HashSet<String>(Arrays.asList(this.roster));
+        
+        
+        for (String name: studentRoster)
         {
-            
+            String[] splitname = name.split(" ");
+            Student newstudent = new Student(splitname[0], splitname[1], 0, chats);
+            students.add(newstudent);
         }
     }
     
